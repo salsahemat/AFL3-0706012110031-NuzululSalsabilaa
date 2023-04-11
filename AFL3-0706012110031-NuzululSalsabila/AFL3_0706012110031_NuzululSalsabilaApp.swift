232744dfb7bbf68e610e16eb6833d2439856124f
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-11 08:38:39 +0700

    Initial Commit

---
+++ AFL3-0706012110031-NuzululSalsabila/AFL3_0706012110031_NuzululSalsabilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110031_NuzululSalsabilaApp.swift
+//  AFL3-0706012110031-NuzululSalsabila
+//
+//  Created by Nuzulul Salsabila on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110031_NuzululSalsabilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL3-0706012110031-NuzululSalsabila/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AFL3-0706012110031-NuzululSalsabila
+//
+//  Created by Nuzulul Salsabila on 11/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
