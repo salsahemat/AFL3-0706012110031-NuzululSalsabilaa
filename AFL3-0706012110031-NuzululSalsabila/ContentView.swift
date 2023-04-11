d498bc08f8bd3c1fbc3c58df090f836178e8e171
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-11 08:53:33 +0700

    section 2

--- AFL3-0706012110031-NuzululSalsabila/ContentView.swift
+++ AFL3-0706012110031-NuzululSalsabila/ContentView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, sift people!")
-        }
-        .padding()
+        Text("Turtle Rock")
+            .font(.title)
+            
     }
 }
 
