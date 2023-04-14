816fc8e2efaa141d68ec76f41a7755a35e81dcdd
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-14 18:16:17 +0700

    Chapter 3 : Section 1

---
+++ AFL3-0706012110031-NuzululSalsabila/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  AFL3-0706012110031-NuzululSalsabila
+//
+//  Created by Nuzulul Salsabila on 14/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
