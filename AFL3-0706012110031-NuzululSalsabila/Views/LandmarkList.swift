5103826e55f230d03122ad5aa87f2a5b00bb142e
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-14 09:20:14 +0700

    Section 4

---
+++ AFL3-0706012110031-NuzululSalsabila/Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  AFL3-0706012110031-NuzululSalsabila
+//
+//  Created by Nuzulul Salsabila on 14/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        //shows the tw landmarks rendered in a list style
+        List{
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
