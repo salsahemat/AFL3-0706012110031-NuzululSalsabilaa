51dbac0f2108830c0c2ff4616057bd264da472bc
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-14 10:31:56 +0700

    Section 6, Chapter 1 done

--- AFL3-0706012110031-NuzululSalsabila/Views/Landmarks/LandmarkDetail.swift
+++ AFL3-0706012110031-NuzululSalsabila/Views/Landmarks/LandmarkDetail.swift
@@ -7,8 +7,16 @@
 
 import SwiftUI
 
+//define landmarkdetail to show detail in landmark
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    //compute the index of the input landmark by comparing it with the model data
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
         ScrollView  {
             MapView(coordinate: landmark.locationCoordinate)
@@ -18,34 +26,40 @@ struct LandmarkDetail: View {
                 .offset(y: -130)
                 .padding(.bottom, -130)
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 HStack {
                     Text(landmark.park)
                     Spacer()
                     Text(landmark.state)
-    
+                    
                 }
                 font(.subheadline)
-                .foregroundColor(.secondary)
+                    .foregroundColor(.secondary)
                 
                 Divider()
-
+                
                 Text("About \(landmark.name)")
-                .font(.title2)
+                    .font(.title2)
                 Text(landmark.description)
-
+                
             }
             .padding()
-           
+            
         }
         .navigationTitle(landmark.name)
         .navigationBarTitleDisplayMode(.inline)
     }
 }
 
+//define LandmarkDetail_Previews to show landmark detail
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
     static var previews: some View {
-        LandmarkDetail(landmark: ModelData().landmarks[0])
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
     }
 }
