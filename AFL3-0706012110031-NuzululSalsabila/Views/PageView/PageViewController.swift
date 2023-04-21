b446ca5e8d29acbebe113a25a7a095a3bdcef21c
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-21 18:47:24 +0700

    Section 2

--- AFL3-0706012110031-NuzululSalsabila/Views/PageView/PageViewController.swift
+++ AFL3-0706012110031-NuzululSalsabila/Views/PageView/PageViewController.swift
@@ -8,21 +8,71 @@
 import SwiftUI
 import UIKit
 
-//stores an array of Page instances
+//manages a UIPageViewController and its data source
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     
     var pages: [Page]
     
+    // creates a Coordinator object that manages the UIPageViewController
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    //initializes and returns a UIPageViewController object with a scroll and a horizontal navigation orientation
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        //data source of UIPageViewController
+        pageViewController.dataSource = context.coordinator
         
         return pageViewController
     }
     
+    //sets the first view controller in the controllers array as the currently displayed view controller
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+    }
+    
+    //providing view controllers to the UIPageViewController object
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        //initialize an array of controllers
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        // returns the view controller that should be displayed before the currently displayed view controller
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+        
+        //eturns the view controller that should be displayed after the currently displayed view controller
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+        
     }
 }
