fe0d92556b18f2fd0b8d3db30db33c9e917c6021
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-23 08:17:56 +0700

    Chapter 4, Section 4: Creating a macOS App

--- AFL3-0706012110031-NuzululSalsabila/Views/Landmarks/LandmarkList.swift
+++ AFL3-0706012110031-NuzululSalsabila/Views/Landmarks/LandmarkList.swift
@@ -12,10 +12,11 @@ struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     //called  showFavoritesOnly set to false to see how the list reacts.
     @State private var showFavoritesOnly = false
+    //initiate filter state variable
     @State private var filter = FilterCategory.all
     @State private var selectedLandmark: Landmark?
     
-    
+    //describe filter states
     enum FilterCategory: String, CaseIterable, Identifiable {
         case all = "All"
         case lakes = "Lakes"
@@ -29,41 +30,24 @@ struct LandmarkList: View {
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
+            && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
     }
     
+    //Returns a string that is used as the title for the navigation bar in the LandmarkList view
     var title: String {
         let title = filter == .all ? "Landmarks" : filter.rawValue
         return showFavoritesOnly ? "Favorite \(title)" : title
     }
     
+    //Returns the index of the selectedLandmark in the modelData.landmarks array
     var index: Int? {
         modelData.landmarks.firstIndex(where: { $0.id == selectedLandmark?.id })
     }
     
     
     var body: some View {
-        //identifiable data by passing along with data key path to a property
-        //        NavigationView {
-        //            List {
-        //                //passing a binding to showFavoritesOnly
-        //                Toggle(isOn: $showFavoritesOnly) {
-        //                    Text("Favorites only")
-        //                }
-        //                ForEach(filteredLandmarks) { landmark in
-        //                    //Specifying the Landmark Detail view
-        //                    NavigationLink{
-        //                        LandmarkDetail(landmark: landmark)
-        //                    } label: {
-        //                        // return a landmark Row from the closure
-        //                        LandmarkRow(landmark: landmark)
-        //                    }
-        //                }
-        //            }
-        //            //set the title of the navbar
-        //            .navigationTitle("Landmarks")
-        //        }
-        //navigation link kemana link akan menuju
+        //navigation stack is the new navigation list
         NavigationStack{
             List(modelData.landmarks) { landmark in
                 NavigationLink{
@@ -72,8 +56,30 @@ struct LandmarkList: View {
                     LandmarkRow(landmark:landmark)
                 }
             }
-            .navigationTitle("Landmarks")
+            .navigationTitle(title)
             .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        //make the filter appear together
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        .pickerStyle(.inline)
+                        
+                        //accessible to see   list of the landmarks gets
+                        Toggle(isOn: $showFavoritesOnly) {
+                            Label("Favorites only", systemImage: "star.fill")
+                        }
+                        
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+            Text("Select a Landmark")
         }
     }
 }
